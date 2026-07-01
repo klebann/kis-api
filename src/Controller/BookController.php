4547b97b6f66588947c1849e525e757497fd7d35
Jakub Kleban <jakub.kleban2000@gmail.com>
2026-07-01 20:50:54 +0200

    Implement full CRUD for Book entity

--- src/Controller/BookController.php
+++ src/Controller/BookController.php
@@ -8,6 +8,7 @@ use App\Repository\BookRepository;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
 class BookController
@@ -53,5 +54,60 @@ class BookController
             'author' => $book->getAuthor(),
             'status' => $book->getStatus()->value,
         ], 201);
+    }
+
+    #[Route('/api/books/{id}', methods: ['GET'])]
+    public function getOne(Book $book): JsonResponse
+    {
+        return new JsonResponse([
+            'id' => $book->getId(),
+            'serialNumber' => $book->getSerialNumber(),
+            'title' => $book->getTitle(),
+            'author' => $book->getAuthor(),
+            'status' => $book->getStatus()->value,
+            'borrowedBy' => $book->getBorrowedBy(),
+            'borrowedAt' => $book->getBorrowedAt()?->format('Y-m-d H:i:s'),
+        ]);
+    }
+
+    #[Route('/api/books/{id}', methods: ['DELETE'])]
+    public function delete(Book $book, EntityManagerInterface $em): JsonResponse
+    {
+        $em->remove($book);
+        $em->flush();
+
+        return new JsonResponse(null, Response::HTTP_NO_CONTENT);
+    }
+
+    #[Route('/api/books/{id}', methods: ['PATCH'])]
+    public function update(
+        Book                   $book,
+        Request                $request,
+        EntityManagerInterface $em
+    ): JsonResponse
+    {
+        $data = json_decode($request->getContent(), true) ?? [];
+
+        if (isset($data['title'])) {
+            $book->setTitle($data['title']);
+        }
+
+        if (isset($data['author'])) {
+            $book->setAuthor($data['author']);
+        }
+
+        if (isset($data['serialNumber'])) {
+            $book->setSerialNumber($data['serialNumber']);
+        }
+
+        $em->flush();
+
+        return new JsonResponse([
+            'id' => $book->getId(),
+            'serialNumber' => $book->getSerialNumber(),
+            'title' => $book->getTitle(),
+            'author' => $book->getAuthor(),
+            'status' => $book->getStatus()->value,
+        ]);
     }
 }
